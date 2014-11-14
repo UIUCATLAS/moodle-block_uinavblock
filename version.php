297aec4c528fc9ff6369658f8b487fcb05340850
nbaxley <nbaxley@illinois.edu>
2014-11-14 11:09:50 -0600

    Chnages to version.php

--- version.php
+++ version.php
@@ -1,3 +1,6 @@
 <?php
 $plugin->version = 2014102911;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->release   = 1.0;       // The human readable version number of the plugin
+$plugin->maturity = MATURITY_STABLE; // This is considered as ready for production sites. 
+$plugin->component = 'block_uinavblock';
